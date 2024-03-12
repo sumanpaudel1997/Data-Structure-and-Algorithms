9352a3c96705c3542b44a7316ca7b586346fce74
Suman Paudel <dastonsuman1997@gmail.com>
2024-03-12 17:51:19 +0545

    dynamic memory allocation

--- C and C++/dynamic_memory_allocation.cpp
+++ C and C++/dynamic_memory_allocation.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void dynamic_calloc()
 {
+    cout << "************************  Implementing calloc() ********************\n\n";
     float *ptr;
     int n;
     float sum = 0, avg = 0;
@@ -11,7 +12,7 @@ void dynamic_calloc()
     cout << "Enter the size of Array: " << endl;
     cin >> n;
     ptr = (float *)calloc(n, sizeof(float));
-    cout << "Enter the elements of array : ";
+    cout << "Enter the elements of array : " << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> ptr[i];
@@ -22,7 +23,7 @@ void dynamic_calloc()
         sum += ptr[i];
         avg = sum / n;
     }
-    cout << "The elements in the array are:";
+    cout << "\nThe elements in the array are: ";
     for (int i = 0; i < n; i++)
     {
         cout << ptr[i] << " ";
@@ -34,14 +35,15 @@ void dynamic_calloc()
 
 void dynamic_malloc()
 {
-    float *ptr, *new_ptr;
+    cout << "************************  Implementing malloc() ********************\n\n";
+    float *ptr;
     int n;
     float sum = 0, avg = 0;
 
     cout << "Enter the size of Array: " << endl;
     cin >> n;
     ptr = (float *)malloc(n * sizeof(float));
-    cout << "Enter the elements of array : ";
+    cout << "Enter the elements of array :" << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> ptr[i];
@@ -52,7 +54,7 @@ void dynamic_malloc()
         sum += ptr[i];
         avg = sum / n;
     }
-    cout << "The elements in the array are:";
+    cout << "\nThe elements in the array are: ";
     for (int i = 0; i < n; i++)
     {
         cout << ptr[i] << " ";
@@ -60,37 +62,67 @@ void dynamic_malloc()
 
     cout << "\nThe total sum of elements of array with dynamic size " << n << " is: " << sum << endl;
     cout << "The average of elements of array is: " << avg << endl;
+    free(ptr);
+}
+
+void dynamic_realloc()
+{
+    cout << "************************  Implementing malloc() ********************\n\n";
+    float *ptr, *new_ptr;
+    int n;
+    float sum = 0, avg = 0;
+
+    cout << "Enter the size of Array: " << endl;
+    cin >> n;
+    ptr = (float *)malloc(n * sizeof(float));
+    cout << "Enter the elements of array :" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ptr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += ptr[i];
+        avg = sum / n;
+    }
+    cout << "\nThe elements in the array are: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << ptr[i] << " ";
+    }
+
+    cout << "\nThe total sum of elements of array with dynamic size " << n << " is: " << sum << endl;
+    cout << "The average of elements of array is: " << avg << endl;
+
+    free(ptr);
 }
 
 int main()
 {
     int choice;
 
-    while (true)
+    cout << "\nEnter choices from (1-4) given below.\n";
+    cout << "1. calloc\n";
+    cout << "2. malloc\n";
+    cout << "3. realloc\n";
+    cout << "4. exit\n\n";
+    cin >> choice;
+
+    switch (choice)
     {
-        cout << "Enter your choice (1-3): ";
-        cin >> choice;
-
-        if (choice < 1 || choice > 3)
-        {
-            cout << "Enter a valid choice from 1 to 3!!" << endl;
-            continue; // Go back to the beginning of the loop
-        }
-
-        switch (choice)
-        {
-        case 1:
-            dynamic_calloc();
-            break;
-        case 2:
-            dynamic_malloc();
-            break;
-        case 3:
-            // Do something for choice 3
-            break;
-        default:
-            break;
-        }
+    case 1:
+        dynamic_calloc();
+        break;
+    case 2:
+        dynamic_malloc();
+        break;
+    case 3:
+        // Do something for choice 3
+        break;
+    case 4:
+        cout << "Program Terminated. Sorry to see you go !!!";
+        exit(1);
     }
 
     return 0;
