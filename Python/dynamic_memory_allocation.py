bafafa5b9bdedd68ab67ba3c4675854a91dd6ad7
Suman Paudel <dastonsuman1997@gmail.com>
2024-03-12 19:18:41 +0545

    dynamic memory allocation

--- Python/dynamic_memory_allocation.py
+++ Python/dynamic_memory_allocation.py
@@ -1,7 +1,7 @@
 
 
 # since python automatically take cares of memory allocation, we need not have to worry.
-# given is the sample way to create the dynamic array or list in python
+# given is the example to create the dynamic array or list in python
 
 size = int(input("Enter the size of an array: "))
 
@@ -15,8 +15,8 @@ for _ in range(0, size):
 
 # accessing array
 print(f'\nThe values in the arrays are:')
-print(*dynamic_array, sep= ", ")
+print(*dynamic_array, sep=", ")
 
 # sum and average of elements of given array.
 print(f'\nThe sum of given dynamic array is: {sum(dynamic_array)}.')
-print(f'The average of given dynamic array is: {round(sum(dynamic_array)/len(dynamic_array),2)}.')
+print(f'The average of given dynamic array is: {round(sum(dynamic_array)/len(dynamic_array), 2)}.')
